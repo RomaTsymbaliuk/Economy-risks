67ac13654caee44fec1128c9f384685af8599eaf
Roman <tsymbaljuk2001@gmail.com>
2023-05-27 21:06:23 +0300

    Initial commit. Write experts risks.

---
+++ main.py
@@ -0,0 +1,6 @@
+import random
+
+risk_list = ["Team extending", "Price of product increasing", "Microchips price increasing", "Terms of programms decreasing", "New bugs appearing", "Electricity blackout", "New concurents appearing", "Minimal salary raising", "New features implementing, product stability integrating"]
+
+def experts_grades_generate():
+
