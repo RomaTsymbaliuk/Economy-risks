81c2adc5509d03f252ff0eadf6e2fe07dfdabc0d
Roman <tsymbaljuk2001@gmail.com>
2023-05-27 21:25:02 +0300

    Used dictionary for risks and grades

--- lab2/main.py
+++ lab2/main.py
@@ -2,5 +2,12 @@ import random
 
 risk_list = ["Team extending", "Price of product increasing", "Microchips price increasing", "Terms of programms decreasing", "New bugs appearing", "Electricity blackout", "New concurents appearing", "Minimal salary raising", "New features implementing, product stability integrating"]
 
-def experts_grades_generate():
+experts_grades = {}
+num_experts = 5
 
+def experts_grades_generate(num_experts):
+    for risk in risk_list:
+        experts_grades[risk] = [random.randint(1,10) for x in range(num_experts)]
+
+experts_grades_generate(num_experts)
+print(experts_grades)
