edc0e09dc64d6762ee14d4ed23783db651f63d24
Roman <tsymbaljuk2001@gmail.com>
2023-05-27 22:52:02 +0300

    Fixed identations

--- lab2/main.py
+++ lab2/main.py
@@ -1,5 +1,6 @@
 import random
 import math
+from decimal import *
 
 risk_list = ["Team extending", "Price of product increasing", "Microchips price increasing", "Terms of programms decreasing", "New bugs appearing", "Electricity blackout", "New concurents appearing", "Minimal salary raising", "New features implementing, product stability integrating"]
 
@@ -8,6 +9,12 @@ num_experts = 5
 sum_index = num_experts
 L_index = sum_index + 1
 L_squared_index = L_index + 1
+
+def print_experts_grades():
+    for grade_key in experts_grades:
+        s = " "
+        string = "  ".join(f'{str(e):<3}' for e in experts_grades[grade_key])
+        print(f"{grade_key : <70}{string : >30}")
 
 def experts_grades_generate(num_experts):
     for risk in risk_list:
@@ -30,15 +37,25 @@ def L_calculate():
     avg = average_calculate()
     for grade_key in experts_grades:
         L = experts_grades[grade_key][sum_index] - avg
-        experts_grades[grade_key].append(L)
+        experts_grades[grade_key].append(round(L, 2))
 
 def L_squared_calculate():
     for grade_key in experts_grades:
         L_squared = math.pow(experts_grades[grade_key][L_index], 2)
-        experts_grades[grade_key].append(L_squared)
+        experts_grades[grade_key].append(round(L_squared,2))
+
+def Concordation_coeff():
+    tv = 0
+    for grade_key in experts_grades:
+        for grade_key2 in experts_grades:
+            if grade_key is not grade_key2 and experts_grades[grade_key][sum_index] == experts_grades[grade_key2][sum_index]:
+                tv = tv + 1
+    tv = tv / 2
+    print('tv is ', tv)
 
 experts_grades_generate(num_experts)
 L_calculate()
 L_squared_calculate()
+print_experts_grades()
 
-print(experts_grades)
+Concordation_coeff()
